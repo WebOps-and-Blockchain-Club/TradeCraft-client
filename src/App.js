b425149ca64e9cac374838b20ac02e736a250f72
smiitm <smitdhameliya2346@gmail.com>
2024-06-07 18:34:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header.js';
+import Landing from './components/Landing.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Landing/>
+    </>
+    
+  );
+}
+
+export default App;
